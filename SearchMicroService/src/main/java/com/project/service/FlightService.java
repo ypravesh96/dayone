a11df181dc9123b11d9a6c7682a3f950fcf8fa17
ypravesh96 <49777614+ypravesh96@users.noreply.github.com>
2021-04-04 09:30:45 +0530

    Add files via upload

---
+++ SearchMicroService/src/main/java/com/project/service/FlightService.java
@@ -0,0 +1,22 @@
+package com.project.service;
+
+import java.util.List;
+
+
+import com.project.model.Flight;
+
+public interface FlightService {
+
+	String addFlight(Flight flight);
+
+	List<Flight> findflights();
+
+	
+
+	Flight updateFlight(Flight flight);
+
+	String removeFlight(String flightNumber);
+
+	Flight getFlight(String flightNumber);
+
+}
